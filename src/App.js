4a81806d4fe0ca7885dd356f665bf8e91c9365ba
Ross Ragsdale <ross.ragsdale@gmail.com>
2019-11-02 14:56:43 -0500

    Add home url.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+const axios = require('axios');
+
+window.extAsyncInit = function() {
+  // the Messenger Extensions JS SDK is done loading 
+  console.log("YOYOYO")
+  console.log(window.name)
+  window.MessengerExtensions.isInExtension()
+
+  let body = {
+    "home_url" : {
+       "url": "https://master.d3hb5qt22e5qrr.amplifyapp.com/",
+       "webview_height_ratio": "tall",
+       "webview_share_button": "show",
+    }
+  }
+  axios.post('https://graph.facebook.com/v5.0/me/messenger_profile?access_token=EAALUbvTt01cBAGs0BIPiE7kLF6xPJMyUcZBwwmKkxQTb5bucHROGe5l4ZChwsibEOhNQr4ezbTiRtOWNNdm0wxzFgWKGH0iM0BVmLMHP7Ei7XuK9VDZAfqNgXs1tExPZCNnYkdNnvTe5cBSE2E4h28POv6v3vYXXdu0Hkd5TkAZDZD', body)
+
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      test
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+(function(d, s, id){
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/messenger.Extensions.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'Messenger'));
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
