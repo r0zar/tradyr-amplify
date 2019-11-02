49f4a569ce3f082a27fb774071ab1165bf90bad0
Ross Ragsdale <ross.ragsdale@gmail.com>
2019-11-02 16:16:30 -0500

    Update.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
 
 window.extAsyncInit = function() {
   // the Messenger Extensions JS SDK is done loading 
@@ -12,15 +14,29 @@ window.extAsyncInit = function() {
 
 function App() {
   return (
-    <Typography component="div" variant="body1">
-      <Box color="primary.main">primary.main</Box>
-      <Box color="secondary.main">secondary.main</Box>
-      <Box color="error.main">error.main</Box>
-      <Box color="text.primary">text.primary</Box>
-      <Box color="text.secondary">text.secondary</Box>
-      <Box color="text.disabled">text.disabled</Box>
-      <Box color="text.hint">text.hint</Box>
-    </Typography>
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <Typography component="div">
+          <Box color="primary.main">primary.main</Box>
+          <Box color="primary.main">primary.main</Box>
+          <Box color="primary.main">primary.main</Box>
+          <Box color="primary.main">primary.main</Box>
+          <Box color="primary.main">primary.main</Box>
+          <Box color="text.primary">text.primary</Box>
+          <Box color="text.primary">text.primary</Box>
+          <Box color="text.primary">text.primary</Box>
+          <Box color="text.primary">text.primary</Box>
+          <Box color="text.primary">text.primary</Box>
+          {/* <Box color="secondary.main">secondary.main</Box>
+          <Box color="error.main">error.main</Box>
+          <Box color="text.primary">text.primary</Box>
+          <Box color="text.secondary">text.secondary</Box>
+          <Box color="text.disabled">text.disabled</Box>
+          <Box color="text.hint">text.hint</Box> */}
+        </Typography>
+      </Container>
+    </React.Fragment>
   );
 }
 
